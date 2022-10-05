4acb92a1f76d54963c74440db1859a5fb97877d2
Ming Hung Chen <minghung@vlsipool-k03.eecs.umich.edu>
2022-10-04 21:46:43 -0400

    remove confidential files

--- apr/platforms/intel16/fastroute.tcl
+++
@@ -1,4 +0,0 @@
-set_global_routing_layer_adjustment m2 0.5 
-set_global_routing_layer_adjustment m3-m7 0.3
-set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) -clock m3-$::env(MAX_ROUTING_LAYER) 
-set_macro_extension 2
--- apr/platforms/intel16/tapCell.tcl
+++
@@ -1,4 +0,0 @@
-tapcell \
-    -distance "77.584" \
-    -tapcell_master "b15ztpn00an1n08x5" \
-    -endcap_master "b15ztpn00an1n08x5"
