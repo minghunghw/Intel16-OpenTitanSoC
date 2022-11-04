2763978ad7694b84a7a395df7b6628288e167414
Ming Hung Chen <minghung@vlsipool-k08.eecs.umich.edu>
2022-11-04 00:09:54 -0400

    modify floorplan to accomodate prsfill cell

--- apr/designs/intel16/top_core/config.mk
+++ apr/designs/intel16/top_core/config.mk
@@ -7,8 +7,9 @@ export SDC_FILE      	= ../syn/$(DESIGN_NAME)/netlist/$(DESIGN_NAME).sdc
 export ADDITIONAL_LEFS 	+= ../ip/memory/ip224uhdlp1p11rf_2048x32m8b2c1s0_t0r0p0d0a1m1h/lef/ip224uhdlp1p11rf_2048x32m8b2c1s0_t0r0p0d0a1m1h.lef
 export ADDITIONAL_GDS	+= ../ip/memory/ip224uhdlp1p11rf_2048x32m8b2c1s0_t0r0p0d0a1m1h/gds/ip224uhdlp1p11rf_2048x32m8b2c1s0_t0r0p0d0a1m1h.gds
 
-export DIE_AREA    = 0 0 1883.52 1961.82
-export CORE_AREA   = 1.26 1.89 1881 1959
+export PRS_FILL    = 1
+export DIE_AREA    = 0 0 1962.36 2042.46
+export CORE_AREA   = 16.2 17.64 1899.72 1979.46
 
 export PLACE_DENSITY = uniform
 export ABC_CLOCK_PERIOD_IN_PS = 1000
--- apr/scripts/floorplan.tcl
+++ apr/scripts/floorplan.tcl
@@ -34,6 +34,18 @@ if {[info exists ::env(FLOORPLAN_DEF)]} {
 # Initialize floorplan using DIE_AREA/CORE_AREA
 # ----------------------------------------------------------------------------
 } else {
+  if {[info exists ::env(PRS_FILL)]} {
+    puts "Insert blockage for prsfill cells placement at (0,0) (58.32 57.96)"
+    odb::dbBlockage_create [ord::get_db_block] 32400 35280 149040 151200
+    puts "Insert obstruction for prsfill cells routing at (0,0) (58.32 57.96)"
+    odb::dbObstruction_create [ord::get_db_block] [[ord::get_db_tech] findLayer m1] 32400 35280 149040 151200
+    odb::dbObstruction_create [ord::get_db_block] [[ord::get_db_tech] findLayer m2] 32400 35280 149040 151200
+    odb::dbObstruction_create [ord::get_db_block] [[ord::get_db_tech] findLayer m3] 32400 35280 149040 151200
+    odb::dbObstruction_create [ord::get_db_block] [[ord::get_db_tech] findLayer m4] 32400 35280 149040 151200
+    odb::dbObstruction_create [ord::get_db_block] [[ord::get_db_tech] findLayer m5] 32400 35280 149040 151200
+    odb::dbObstruction_create [ord::get_db_block] [[ord::get_db_tech] findLayer m6] 32400 35280 149040 151200
+    odb::dbObstruction_create [ord::get_db_block] [[ord::get_db_tech] findLayer m7] 32400 35280 149040 151200
+  }
   initialize_floorplan -die_area $::env(DIE_AREA) \
                        -core_area $::env(CORE_AREA) \
                        -site $::env(PLACE_SITE)
