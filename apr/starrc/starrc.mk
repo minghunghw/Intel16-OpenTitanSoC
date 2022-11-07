913e9a34c70efc54d41721cfa030466b3e6ce1dd
Ming Hung Chen <minghung@vlsipool-k15.eecs.umich.edu>
2022-11-06 20:50:11 -0500

    add spef file to target finish in Makefile

--- apr/starrc/starrc.mk
+++
@@ -1,5 +0,0 @@
-
-extraction: finish
-	rm -rf starrc/extraction && mkdir -p starrc/extraction && cd starrc/extraction && \
-	envsubst < ../star_rcxt.asic.cmd > run.cmd && \
-	StarXtract -clean run.cmd
